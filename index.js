b115a9b242722ddf8fa2107fa14a1127be85868d
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2025-08-23 15:42:53 +0530

    solved cors error

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(
   cors({
-    origin: "https://www.adityakhadeak.engineer",
+    // origin: "https://www.adityakhadeak.engineer",
+          origin: "https://www.adityakhade.tech",
+
   })
 );app.use('/api/contact',routeContact)
 app.use('/api/user',routeUser)
